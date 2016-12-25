428195f2e90d1107e138c41182d191e670bd60ff
huangjilaiqin <1577594730@qq.com>
2016-12-25 10:19:36 +0800

    fix error:constructor ReadableNativeMap in class ReadableNativeMap cannot be applied to given types

--- android/src/main/java/com/gcrabtree/rctsocketio/SocketIoReadableNativeMap.java
+++ android/src/main/java/com/gcrabtree/rctsocketio/SocketIoReadableNativeMap.java
@@ -4,6 +4,7 @@ import android.util.Log;
 
 import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.bridge.ReadableNativeMap;
+import com.facebook.jni.HybridData;
 
 import java.util.HashMap;
 
@@ -15,6 +16,10 @@ import io.socket.client.IO;
 
 public class SocketIoReadableNativeMap extends ReadableNativeMap {
     private static final String TAG = "SIOReadableNativeMap";
+
+    protected SocketIoReadableNativeMap(HybridData hybridData) {
+      super(hybridData);
+    }
     /**
      * Note: This will only be necessary until RN version 0.26 goes live
      * It will be deprecated from the project, as this is just included in that version of RN.
